c6201c1a266d9ea2128232c7192bdbeaba745842
Nick Mansell <nick.mansell@gmail.com>
2019-11-28 11:46:37 -0500

    Update getip.js

--- getip.js
+++ getip.js
@@ -8,6 +8,7 @@ if (this.readyState == 4 && this.status == 200) {
       var result = JSON.parse(this.responseText);
       document.write("IP ADDRESS: " + result.ip_address);
       console.log(result);
+      console.log(result.ip_address);
     }
 };
 
